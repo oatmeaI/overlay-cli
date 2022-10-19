bb5bab5c765a42e4a900bf38807e627a97e90ce7
Keenan Villani-Holland <villanih@adobe.com>
2022-10-19 14:16:24 -0700

    clean up and add comments

--- overlaycli/main.swift
+++ overlaycli/main.swift
@@ -1,6 +1,11 @@
 import AppKit
 import Cocoa
 
+// overlay-cli: A commandline tool that displays an overlay onscreen with the given text
+
+// We're creating a headless app - there's no Dock icon or menubar or anything
+// So we need to override some properties of NSWindow (the Cocoa base class for a window)
+// in order to make it so that this weird non-app window is allowed to be focused
 class SWindow: NSWindow {
     override var canBecomeKey:Bool {
         return true
@@ -11,62 +16,54 @@ class SWindow: NSWindow {
     }
 }
 
+// This class describes the actual app itself!
 class Overlay: NSObject, NSApplicationDelegate, NSWindowDelegate {
     var window: NSWindow!
     
     func main() {
-        NSApp.activate(ignoringOtherApps: true)
+        NSApp.activate(ignoringOtherApps: true) // Brings our app to the front
         
-        let winRect:NSRect = NSMakeRect(0, 0, 100, 100)
-        self.setupWindow(winRect: winRect)
-        self.window.center()
-        self.window.makeKeyAndOrderFront(nil)
-        self.window.orderFrontRegardless()
+        let startingRect = NSMakeRect(0, 0, 500, 500) // Create the rectangle that our window will use (we will want to pass the dimensions in as arguments in the future)
+        self.setupWindow(winRect: startingRect) // winRect: startingRect - this is a labeled argument.
+        
+        self.window.center() // duh
+        self.window.makeKeyAndOrderFront(nil) // Might not be necessary; more stuff to force our window on top of everything else.
+        self.window.orderFrontRegardless() // Brings our WINDOW to the front (as opposed to line 24)
     }
     
-    func setupWindow(winRect: NSRect) {
-        let styleMask = NSWindow.StyleMask.borderless
-        self.window = SWindow(contentRect: winRect, styleMask: styleMask, backing: NSWindow.BackingStoreType.buffered, defer: false)
-        self.window.isOpaque = false
-        self.window.alphaValue = 1
-        self.window.backgroundColor = NSColor.clear
-        self.window.delegate = self
+    func setupWindow(winRect: NSRect) { // winRect: NSRect - this defines a labeled argument, and defines the type of the argument as NSRect
+        let styleMask = NSWindow.StyleMask.borderless // Just a builtin constant that tells the window not to draw the usual window chrome
+        self.window = SWindow(contentRect: winRect, styleMask: styleMask, backing: NSWindow.BackingStoreType.buffered, defer: false) // Create an instance of our custom window class. Pass in the rectangle, the styleMask we defined. unclear what backing and defer mean; this is copypasta code lol
+        self.window.isOpaque = false // duh
+        self.window.backgroundColor = NSColor.clear // The window itself shouldn't have a background...you'll see why
+        self.window.delegate = self // dunno. possibly not needed.
         
-        let height = 500.0
-        let width = 500.0
+        self.window.setFrame(winRect, display: true) // also unclear what this does lol
         
-        let winRect: NSRect = NSMakeRect(0, 0, width, height)
-        self.window.setFrame(winRect, display: true)
+        let message = NSView(frame: winRect) // create a view that will hold our content
+        message.wantsLayer = true // `layer` means a Core Animation layer - this is how we will give it rounded corners
+        message.layer?.cornerRadius = 10 // voila
         
-        let message = NSView(frame: winRect)
-        message.wantsLayer = true
-        message.layer?.cornerRadius = 10
+        let messageText = NSText(frame: winRect) // now create the object that holds the text we wanna render
+        messageText.string = "HERE SOME TEXT" // we'll also want to pass this in somehow
+        messageText.backgroundColor = NSColor.init(red: 0.5, green: 0.2, blue: 0.9, alpha: 0.8) // gonna want to pass this in as well
         
-        let messageText = NSText(frame: winRect)
-        messageText.string = "HERE SOME TEXT"
-        messageText.backgroundColor = NSColor.init(red: 0.5, green: 0.2, blue: 0.9, alpha: 0.8)
+        message.addSubview(messageText) // add the text to the Core Animation layer we created above...
         
-        message.addSubview(messageText)
-        
-        self.window.contentView?.addSubview(message)
-        
-        let v: NSView = NSView(frame: winRect)
-        v.wantsLayer = true
-        v.layer?.backgroundColor = NSColor.green.cgColor
-//        self.window?.contentView?.addSubview(v)
+        self.window.contentView?.addSubview(message) // ...and then add the layer to window
     }
 }
 
 func main() -> Int {
-    autoreleasepool {
-        let delegate = Overlay()
-        let app = NSApplication.shared
+    autoreleasepool { // dunno what this does, I assume turns on automatic memory management / garbage collection
+        let delegate = Overlay() // Create our app object
+        let app = NSApplication.shared // NSApplication.shared is like...a reference to the Application object provided by AppKit? Honestly I don't quite understand this stuff...
         
-        app.delegate = delegate
-        delegate.main()
+        app.delegate = delegate // ...but attaching our Overlay instance to NSApplication.shared.delegate is kinda what...makes the app run.
+        delegate.main() // This makes sense, we're calling the actual method we wrote above...
         
-        _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
+        _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv) // ...but the app also doesn't run if we don't also call this. Frankly, I think I'm doing something wrong here, but I don't quite understand what.
     }
     return 0
 }
-_ = main()
+_ = main() // aaaand just run the actual program.
